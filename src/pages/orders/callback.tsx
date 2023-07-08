1d1757bc5abfc6a02323733448a03306a9e7dc75
Can Burak S <canburak@msn.com>
2023-07-09 02:19:02 +0300

    Update callback.tsx

--- src/pages/orders/callback.tsx
+++ src/pages/orders/callback.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import nextI18NextConfig from '@/../next-i18next.config';
-import { LocaleType, OrderQueryResponse, OrderQueryApiResponse } from '@/types';
+import { LocaleType, OrderQueryApiResponse } from '@/types';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { IYZICO_LOCAL_STORAGE_TOKEN, IYZICO_QUERY_RESPONSE_ENDPOINT } from '@/constants';
 import { getUrl, parseOrderFromIyzicoResponse } from '@/utils';
@@ -43,34 +43,39 @@ export const getServerSideProps = async ({
       const orderData = parseOrderFromIyzicoResponse(orderResponse);
       if (orderData) {
         const client = getSupabaseBrowserClient();
-        await client.from('orders').upsert(orderData).select('*');
+        const result = await client.from('orders').upsert(orderData).select('*');
+        return result
       }
     };
-    fetch(getUrl() + IYZICO_QUERY_RESPONSE_ENDPOINT, {
+    return fetch(getUrl() + IYZICO_QUERY_RESPONSE_ENDPOINT, {
       method: 'POST',
       body: JSON.stringify({ token }),
     }).then((res: Response) => res.json())
-      .then((res: OrderQueryResponse | { [message: string]: string }) => {
-        console.log(`api response order query response: ${JSON.stringify(res)}`);
+      .then(async (res: OrderQueryApiResponse | { [message: string]: string }) => {
+        // console.log(`api response order query response: ${JSON.stringify(res)}`);
         /**
          * Save the returned data to Supabase
          * We redirect user to the corresponding checkout/token page.
          */
-        if (res.token) {
-          const resAnyType = res as any;
-          setOrderResponse(res.token, resAnyType);
+          const orderSetResponse = await setOrderResponse(res.token, res);
+          const jsonOrderSetResponse = await orderSetResponse;
+          console.log(jsonOrderSetResponse);
+          return {
+            props: {
+              token: token,
+              ...(await serverSideTranslations(locale, ['common'], nextI18NextConfig)),
+            },
         }
       })
-      .catch((e) => {
+      .catch(async (e) => {
         console.error('Callback page server props token error:', e);
+        return {
+          props: {
+            token: token,
+            ...(await serverSideTranslations(locale, ['common'], nextI18NextConfig)),
+          },
+        };
       });
-
-    return {
-      props: {
-        token: token,
-        ...(await serverSideTranslations(locale, ['common'], nextI18NextConfig)),
-      },
-    };
   }
   return {
     token: undefined,

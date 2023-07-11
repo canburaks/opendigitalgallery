40d2093a2a14b7c981c31267b2905fa9db471f22
anil <anilakgunes@gmail.com>
2023-07-11 21:03:52 +0300

    test husky

--- src/views/CheckoutView/forms/CheckoutAddressForm.tsx
+++ src/views/CheckoutView/forms/CheckoutAddressForm.tsx
@@ -48,7 +48,6 @@ export const CheckoutAddressForm = forwardRef<RefType>((props, ref) => {
     zip: address?.zip || '',
     country_id: address?.country_id.toString() || '',
   };
-  console.log('');
   return (
     <Formik
       innerRef={ref}
